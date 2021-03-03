e4609cb9002bbb4d40632c5bc3fef0e3475151b6
TheAndreyy <andrzej.kusiak00@gmail.com>
2021-03-03 19:46:55 +0100

    Fixing bug in loop range

--- qsort.c
+++ qsort.c
@@ -11,7 +11,7 @@ void quickSort(int a[], int p, int r) {
 int partition(int a[], int p, int r) {
     int x = a[r];
     int i = p - 1;
-    for(int j = p; j < r - 1; j++) {
+    for(int j = p; j < r; j++) {
         if(a[j] <= x) {
             i++;
             swap(a+i, a+j);
