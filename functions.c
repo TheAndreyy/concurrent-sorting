6a3fadb72961fe90b2d4459561a109eb46bba988
TheAndreyy <andrzej.kusiak00@gmail.com>
2021-03-03 19:48:23 +0100

    Adding supporting funtions

---
+++ functions.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int getRandom(int min, int max) {
+    return random() % (max + 1 - min) + min;
+}
+
+int showArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int* createArray(int size, int min, int max) {
+    int *ptr = (int*)malloc(size * sizeof(int));
+    for(int i = 0; i < size; i++) {
+        ptr[i] = getRandom(min, max);
+    }
+    return ptr;
+}
---
+++ functions.h
@@ -0,0 +1,8 @@
+
+int getRandom(int min, int max);
+
+int showArray(int arr[], int size);
+
+int* createArray(int size, int min, int max);
+// pass size of array, minimal and maximal value of ellements in array
+// array will be filled with elements in range from min to max
