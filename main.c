f6769bd54110c1e8aca0f5f59ae0f4000d415622
TheAndreyy <andrzej.kusiak00@gmail.com>
2021-03-03 19:49:20 +0100

    Creating main to test qsort

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include "qsort.h"
+#include "functions.h"
 
 
 int main(int argc, char *argv[]) {
-    printf("Hello World!");
+    if(argc != 2) {
+        printf("Enter size of an array\n");
+        exit(-1);
+    }
+
+    srand(time(NULL));
+
+    int size = atoi(argv[1]);
+
+    int *arr = createArray(size, 0, 100*size);
+
+    showArray(arr, size);
+
+    quickSort(arr, 0, size-1);
+
+    showArray(arr, size);
+
+    free(arr);
 }
