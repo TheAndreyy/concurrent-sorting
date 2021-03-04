68568acad50cdf4fcb615191736b30b4e269f480
TheAndreyy <andrzej.kusiak00@gmail.com>
2021-03-04 02:20:48 +0100

    Testing qsort-process in main

--- src/main.c
+++ src/main.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/mman.h>
 
 #include "qsort.h"
 #include "functions.h"
 #include "qsort-thread.h"
+#include "qsort-process.h"
 
 
 int main(int argc, char *argv[]) {
@@ -20,12 +22,15 @@ int main(int argc, char *argv[]) {
     int size = atoi(argv[1]);
 
     int *arr1 = createArray(size, 0, 100*size);
-    int *arr2 = (int*) malloc(size * sizeof(int));
 
+    int *arr2 = (int*) malloc(size * sizeof(int));
     memcpy(arr2, arr1, size * sizeof(int));
 
-    // showArray(arr1, size);
-    // showArray(arr2, size);
+    int protection = PROT_READ | PROT_WRITE;
+    int visibility = MAP_SHARED | MAP_ANONYMOUS;
+    int *arr3 = (int*) mmap(NULL, size * sizeof(int), protection, visibility, -1, 0);
+
+    memcpy(arr3, arr1, size * sizeof(int));
 
     struct timeval start, end;
 
@@ -41,6 +46,9 @@ int main(int argc, char *argv[]) {
 
     printf("Time for Quick Sort: %lld us\n", micros);
 
+    free(arr1);
+
+
     threadArgs args = {arr2, 0, size-1, atoi(argv[2])};
 
     gettimeofday(&start, NULL);
@@ -54,9 +62,20 @@ int main(int argc, char *argv[]) {
 
     printf("Time for Quick Sort with threads: %lld us\n", micros);
 
-    // showArray(arr1, size);
-    // showArray(arr2, size);
-
-    free(arr1);
     free(arr2);
+
+
+    gettimeofday(&start, NULL);
+
+
+    quickSortProcess(arr3, 0, size-1, atoi(argv[2]));
+
+    gettimeofday(&end, NULL);
+
+    seconds = (end.tv_sec - start.tv_sec);
+    micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+
+    printf("Time for Quick Sort with processes: %lld us\n", micros);
+
+    munmap(arr3, size);
 }
